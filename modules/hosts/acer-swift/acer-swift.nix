7e5bd38ab5a31299fcb107921e41a8888ffbeca0
xiaot-evo <3258412091@qq.com>
2026-05-25 19:52:07 +0800

    refactor(acer-swift): add hostname battery to includes pattern

--- modules/hosts/acer-swift/acer-swift.nix
+++ modules/hosts/acer-swift/acer-swift.nix
@@ -4,18 +4,23 @@
   den.hosts.x86_64-linux.acer-swift.users.xiaot_evo = { };
 
   den.aspects.acer-swift = {
-    includes = with den.aspects; [
-      acer-swift.hardware
-      (system.hardware.nvidia {
-        nvidiaBusId = "PCI:1@0:0:0";
-        amdgpuBusId = "PCI:4@0:0:0";
-      })
-      (system.hardware.nbfc-linux "Acer Swift SFX14-41G")
-      (system.boot "/boot/efi")
-      system.nix
-      system.sound
-    ];
+    includes =
+      (with den.batteries; [
+        hostname
+      ])
+      ++ (with den.aspects; [
+        acer-swift.hardware
+        (system.hardware.nvidia {
+          nvidiaBusId = "PCI:1@0:0:0";
+          amdgpuBusId = "PCI:4@0:0:0";
+        })
+        (system.hardware.nbfc-linux "Acer Swift SFX14-41G")
+        (system.boot "/boot/efi")
+        system.nix
+        system.sound
+      ]
 
+      );
     nixos =
       { pkgs, lib, ... }:
       {
