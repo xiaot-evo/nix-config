50e33c270745a3c5dd24f89b1518931690a1c978
xiaot-evo <3258412091@qq.com>
2026-07-02 23:14:05 +0800

    feat(zed): replace nixd package with nil

--- modules/features/dev/editors/zed-editor/zed-editor.nix
+++ modules/features/dev/editors/zed-editor/zed-editor.nix
@@ -12,7 +12,7 @@
           enable = true;
           package = pkgs.zed-editor;
           extraPackages = with pkgs; [
-            nixd
+            nil
             nixfmt
             package-version-server
             yaml-language-server
