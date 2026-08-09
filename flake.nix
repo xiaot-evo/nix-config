ae06b14f2fb7e5adf5886fc007638d007a0b9e48
xiaot-evo <3258412091@qq.com>
2026-08-10 00:56:29 +0800

    chore(deps): 移除 zen-browser flake 输入（改用官方 flatpak 版）

--- flake.nix
+++ flake.nix
@@ -73,12 +73,5 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        home-manager.follows = "home-manager";
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
   };
 }
